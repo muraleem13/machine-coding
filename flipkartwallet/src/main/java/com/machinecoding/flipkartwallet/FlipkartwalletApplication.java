b0dd7669b047287f7e5fb7a579b9098c4f6bf3e6
muraleem13 <muralee.m13@gmail.com>
2026-03-24 22:37:55 +0530

    initial commit

---
+++ flipkartwallet/src/main/java/com/machinecoding/flipkartwallet/FlipkartwalletApplication.java
@@ -0,0 +1,13 @@
+package com.machinecoding.flipkartwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartwalletApplication.class, args);
+	}
+
+}
